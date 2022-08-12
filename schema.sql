ade3e036a320c2ee762e8c68ac1a7ffabb5a801b
Kweku Bright <kweku01bright@gmail.com>
2022-08-12 06:28:08 +0100

    Format Queries

--- schema.sql
+++ schema.sql
@@ -1,10 +1,10 @@
 /* Database schema to keep the structure of entire database. */
-
 CREATE TABLE animals (
-    id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY ,
-    name TEXT,
-    date_of_birth DATE,
-    escape_attempts INTEGER,
+    id INT GENERATED ALWAYS AS IDENTITY,
+    name VARCHAR(100) NOT NULL,
+    date_of_birth DATE NOT NULL,
+    escape_attempts INT,
     neutered BOOLEAN,
-    weight_kg DECIMAL
+    weight_kg DECIMAL NOT NULL,
+    PRIMARY KEY(id)
 );
