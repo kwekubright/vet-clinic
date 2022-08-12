64096e1f81e7a0bebaf62f7ff5e6daaa8808ff50
Kweku Bright <kweku01bright@gmail.com>
2022-08-12 17:18:52 +0100

    Query multiple table

--- queries.sql
+++ queries.sql
@@ -8,20 +8,35 @@ SELECT name FROM animals WHERE neutered = true;
 SELECT name FROM animals WHERE name != 'Gabumon';
 SELECT name FROM animals WHERE weight_kg BETWEEN 10.4 AND 17.3;
 
--- How many animals are there?
 SELECT COUNT(*) FROM animals;
 
--- How many animals have never tried to escape?
 SELECT COUNT(*) FROM animals WHERE escape_attempts = 0;
 
--- What is the average weight of animals?
 SELECT AVG(weight_kg) FROM animals;
 
--- Who escapes the most, neutered or not neutered animals?
 SELECT name FROM animals WHERE escape_attempts = (SELECT MAX(escape_attempts) FROM animals) neutered = true OR neutered = false;
 
--- What is the minimum and maximum weight of each type of animal?
 SELECT species, MIN(weight_kg), MAX(weight_kg) FROM animals GROUP BY species;
 
--- What is the average number of escape attempts per animal type of those born between 1990 and 2000?
 SELECT species, AVG(escape_attempts) FROM animals WHERE date_of_birth BETWEEN '1990-01-01' AND '2000-12-31' GROUP BY species;
+
+-- What animals belong to Melody Pond? 
+SELECT animals.name FROM animals JOIN owners ON animals.owner_id = owners.id WHERE owners.full_name = 'Melody Pond';
+
+-- List of all animals that are pokemon (their type is Pokemon).
+SELECT animals.name FROM animals JOIN species ON species.id = animals.species_id WHERE species.name = 'Pokemon';
+
+-- List all owners and their animals, remember to include those that don't own any animal.
+SELECT owners.full_name, animals.name FROM owners JOIN animals ON owners.id = animals.owner_id;
+
+-- How many animals are there per species?
+SELECT species.name, COUNT(*) FROM species JOIN animals ON species.id = animals.species_id GROUP BY species.name;
+
+-- List all Digimon owned by Jennifer Orwell.
+SELECT animals.name FROM animals JOIN owners ON owners.id = animals.owner_id WHERE owners.full_name = 'Jennifer Orwell';
+
+-- List all animals owned by Dean Winchester that haven't tried to escape.
+SELECT animals.name FROM animals JOIN owners ON owners.id = animals.owner_id WHERE owners.full_name = 'Dean Winchester' AND animals.escape_attempts = 0;
+
+-- Who owns the most animals?
+SELECT owners.full_name, COUNT(*) FROM owners JOIN animals ON owners.id = animals.owner_id GROUP BY owners.full_name ORDER BY COUNT(*) DESC;
--- schema.sql
+++ schema.sql
@@ -10,3 +10,36 @@ CREATE TABLE animals (
 );
 
 ALTER TABLE animals ADD species VARCHAR(100);
+
+/* Create a table named owners with the following columns:
+id: integer (set it as autoincremented PRIMARY KEY)
+full_name: string
+age: integer */
+
+CREATE TABLE owners (
+    id INT GENERATED ALWAYS AS IDENTITY,
+    full_name VARCHAR(100) NOT NULL,
+    age INT NOT NULL,
+    PRIMARY KEY(id)
+);
+
+/* Create a table named species with the following columns:
+id: integer (set it as autoincremented PRIMARY KEY)
+name: string */
+
+CREATE TABLE species (
+    id INT GENERATED ALWAYS AS IDENTITY,
+    name VARCHAR(100) NOT NULL,
+    PRIMARY KEY(id)
+);
+
+/* Modify animals table:
+Make sure that id is set as autoincremented PRIMARY KEY
+Remove column species
+Add column species_id which is a foreign key referencing species table
+Add column owner_id which is a foreign key referencing the owners table */
+/* REmove column species from animals table */
+ALTER TABLE animals DROP COLUMN species;
+ALTER TABLE animals ADD COLUMN species_id INT REFERENCES species;
+ALTER TABLE animals ADD COLUMN owner_id INT REFERENCES owners;
+
